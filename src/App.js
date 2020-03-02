543ee432c00ad1eb9a3c263978eec47e01ecc06f
raphaelbachega <raphael.messias@bandtec.com.br>
2020-03-02 12:33:56 -0300

    AttSysdrink

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [start, setStart] = useState(false);
 
   const initiate = () => {
-    setStart(true);
+    setStart(!start);
   }
 
   return (
@@ -16,7 +16,10 @@ function App() {
       <Style.FigureRight start={start}/>
       <Style.FigureLeft start={start}/>
       <Style.Title start={start}>SysDrink&Food</Style.Title>
-      <Style.Button onClick={initiate}>Iniciar</Style.Button>
+      <Style.Welcome>Bem vindo!</Style.Welcome>
+      <Style.Greetings>Como é bom tê-lo conosco!</Style.Greetings>
+      <Style.Button start={start} onClick={initiate}>Iniciar</Style.Button>
+      <Style.Box start={start}></Style.Box>
     </>
   );
 }
--- src/style.js
+++ src/style.js
@@ -49,7 +49,7 @@ export const Name = styled.div`
     width: 25VW;
     height: 30VH;
     opacity: ${props => props.start ? '0' : '1'};
-    transition: opacity 1s;
+    transition: opacity 0.5s;
 
     @media(min-width: ${size}){
         font-size: 3VW;
@@ -67,12 +67,13 @@ export const Title = styled.div`
 
     @media(min-width: ${size}){
         font-size: 6VW;
-        top: ${props => props.start ? '3VH' : 'calc(50VH - 6VW)'}; 
+        top: ${props => props.start ? '3VH' : "9VH"}; 
     }
 `
   
 export const Button = styled.div`
-    top: 50VH;
+
+    top: 70VH;
     left: 15VW;
     position: absolute;
     width: 70VW;
@@ -92,3 +93,48 @@ export const Button = styled.div`
         font-size: 5VW;
     }
 `
+export const Welcome = styled.div`
+    top: 50VH;
+    position: absolute;
+    width: 100VW;
+    text-align: center;
+    font-size:10VW;
+    color: #1f2a44;
+
+    @media(min-width: ${size}){
+        top: 30VH;
+        position: absolute;
+        width: 100VW;
+        text-align: center;
+        font-size:4VW;
+        color: #1f2a44;
+    }
+`;
+
+export const Greetings = styled.div`
+    top: 57VH;
+    position: absolute;
+    width: 100VW;
+    text-align: center;
+    font-size: 6VW;
+    color: #63666a;
+
+    @media(min-width: ${size}){
+        top: 40VH;
+        position: absolute;
+        width: 100VW;
+        text-align: center;
+        font-size: 3VW;
+        color: #63666a;
+    }
+`;
+
+export const Box = styled.div`
+    position: absolute;
+    top: ${props => props.start ? '20VH' : '40VH'};
+    width: 100VW;
+    height: 60VH;
+    background-color: white;
+    z-index: -10;
+    transition: top 1s;
+`;
